1ca2e5a34403ca51807b19d0fdad7968798979c0
CHINMAY <chinmaysri25@gmail.com>
2020-09-28 01:54:17 +0530

    Updates

---
+++ precache-manifest.45f74679cb9a75f7fd71a3565cc60a48.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68191f9a15422415435567c413372cd3",
+    "url": "/certificatePortal/index.html"
+  },
+  {
+    "revision": "06f0a7d296663da815d3",
+    "url": "/certificatePortal/static/css/main.3aca546e.chunk.css"
+  },
+  {
+    "revision": "7a79706f9b997c370aa8",
+    "url": "/certificatePortal/static/js/2.0327bfc7.chunk.js"
+  },
+  {
+    "revision": "32c7bca6f68b346eef00",
+    "url": "/certificatePortal/static/js/3.d0fbc576.chunk.js"
+  },
+  {
+    "revision": "4cee33b7f3f8ffad12f0a8d281f6cfa4",
+    "url": "/certificatePortal/static/js/3.d0fbc576.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7221d680872f5018f9ef",
+    "url": "/certificatePortal/static/js/4.eeff9522.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/certificatePortal/static/js/4.eeff9522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f0cae76e0a4675b8235",
+    "url": "/certificatePortal/static/js/5.a5149e50.chunk.js"
+  },
+  {
+    "revision": "3a7bc1760b9d274e2e5e7531bce86076",
+    "url": "/certificatePortal/static/js/5.a5149e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06f0a7d296663da815d3",
+    "url": "/certificatePortal/static/js/main.272f56c3.chunk.js"
+  },
+  {
+    "revision": "4346b9af1b43104b8cf2",
+    "url": "/certificatePortal/static/js/runtime-main.2f1e750f.js"
+  },
+  {
+    "revision": "9d1665a8481cae664d1481b3e2036b48",
+    "url": "/certificatePortal/static/media/foxplay.9d1665a8.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/certificatePortal/static/media/logo.5d5d9eef.svg"
+  }
+]);
